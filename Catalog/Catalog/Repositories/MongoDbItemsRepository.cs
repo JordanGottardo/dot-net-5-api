0a83a2e781c1fa90c1e9f4f29e74613c2d6c1592
JordanG <jordangottardo@libero.it>
2022-02-04 16:03:58 +0100

    implement mongodb repository

---
+++ Catalog/Catalog/Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        #region Private Fields
+
+        private const string DatabaseName = "catalog";
+        private const string CollectionName = "items";
+        private readonly IMongoCollection<Item> _itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        #endregion
+
+        #region Initialization
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(DatabaseName);
+            _itemsCollection = database.GetCollection<Item>(CollectionName);
+        }
+
+        #endregion
+
+        public IEnumerable<Item> GetItems()
+        {
+            return _itemsCollection.Find(new BsonDocument()).ToList();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return _itemsCollection.Find(filter).SingleOrDefault();
+        }
+
+        public void CreateItem(Item item)
+        {
+            _itemsCollection.InsertOne(item);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+            _itemsCollection.ReplaceOne(filter, item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id, id);
+            _itemsCollection.DeleteMany(filter);
+        }
+    }
+}
--- Catalog/Catalog/Startup.cs
+++ Catalog/Catalog/Startup.cs
@@ -12,6 +12,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Catalog.Repositories;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 namespace Catalog
 {
@@ -27,7 +31,15 @@ namespace Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            services.AddSingleton<IMongoClient>(serviceProvider =>
+            {
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
+            services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
